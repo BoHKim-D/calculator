9acafabe8b686a0d4242d48f0490020dd09d0cf2
BoHKim <91491714+BoHKim-D@users.noreply.github.com>
2022-02-08 12:54:25 -0800

    Update calc.js

--- calc.js
+++ calc.js
@@ -15,8 +15,8 @@ reader.question("What would you like to calculate?", function(input){
 	num2 = Number(tokens[2]);
 
 	console.log('mathSymbol', mathSymbol);
-    console.log('num1', num1);
-    console.log('num2', num2);
+    	console.log('num1', num1);
+    	console.log('num2', num2);
 
 	if (mathSymbol === '+') {
 		console.log(num1 + num2);
